3bf56594f3c8baa4ff0cf530bfbd093afa946c5b
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-06-26 03:34:24 -0400

    commit

--- dags/etl_pipeline_smoking_cost.py
+++ dags/etl_pipeline_smoking_cost.py
@@ -9,31 +9,41 @@ import logging
 import pandas as pd
 import pendulum
 
-
+# Logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 url = 'https://data.cdc.gov/resource/ezab-8sq5.json'
 
 # Define the tasks
 def extract_task(url):
-        re = requests.get(url)
-        json_data = re.json()
-        return json_data
+        try:
+                re = requests.get(url)
+                json_data = re.json()
+                logging.info('Data extracted successfully')
+                return json_data
+        except requests.exceptions.RequestException as e:
+                logging.error('Failed to extract data')
+                return None
 
-data = extract_task('https://data.cdc.gov/resource/ezab-8sq5.json')
+# data = extract_task('https://data.cdc.gov/resource/ezab-8sq5.json')
 
 def transform_task(data):
-        # Convert the json_data to a pandas dataframe
-        df = pd.DataFrame(data)
-        # print(df)
-        return df
+        try:
+                # Convert the json_data to a pandas dataframe
+                df = pd.DataFrame(data)
+                logging.info('Data transformed successfuly')
+                return df
+        except Exception as e:
+                logging.error('Failed to transform data')
+                return None
 
-print(transform_task(data))
+# print(transform_task(data))
 
 def load_task(data):
         pass
 
 # Define the Dag
 with DAG("etl_pipeline_smoking_cost", start_date=datetime(2024, 1, 1),
-        schedule_interval="@daily", catchup=False) as dag:
+        schedule_interval="@yearly", catchup=False) as dag:
 
         extract_task_01 = PythonOperator(
                 task_id="extract_task_01",
@@ -44,13 +54,13 @@ with DAG("etl_pipeline_smoking_cost", start_date=datetime(2024, 1, 1),
         transform_task_01 = PythonOperator(
                 task_id="transform_task_01",
                 python_callable=transform_task,
-                op_args=['{{ task_instance.xcom_pull(task_ids="extract_task_01") }}']
+                op_args=['{{ ti.xcom_pull(task_ids="extract_task_01") }}']
         )
 
         load_task_01 = PythonOperator(
                 task_id="load_task_01",
                 python_callable=load_task,
-                op_args=['{{ task_instance.xcom_pull(task_ids="transform_task_01") }}']
+                op_args=['{{ ti.xcom_pull(task_ids="transform_task_01") }}']
         )
 
         extract_task_01 >> transform_task_01 >> load_task_01
